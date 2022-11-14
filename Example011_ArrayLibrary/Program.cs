d6b56c84f05dcff09db2cc0ec58e7542317318d4
RenatDavletshin <immortal1307@gmail.com>
2022-11-14 21:46:01 +0500

    Example010End

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+void FillArray(int[] collection)  // метод, который ничего не возвращает, return не используем
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
